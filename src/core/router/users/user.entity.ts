afc5c7dc1671ba5f84774754d98f76597036d582
Ha_LN <ha_ln@EVN084-HaLN.local>
2021-07-02 15:20:43 +0700

    changr start

--- src/core/router/users/user.entity.ts
+++ src/core/router/users/user.entity.ts
@@ -1,7 +1,7 @@
 import { BaseEntity } from 'src/utils/base.entity'
 import { Entity, Column, OneToMany} from 'typeorm'
 import { Blog } from '../blog/blog.entity'
-@Entity()
+@Entity('users')
 export class User extends BaseEntity {
   @Column()
   email: string
